3f9b7e370f2053dc184c4767801ad025b8aeac8d
Prabhdeep Singh <67806882+AospRemixer@users.noreply.github.com>
2021-05-20 14:33:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+from KahootBot import KahootBot
+kahoot_bot = KahootBot()
+kahoot_bot.play(kahoot_nickname="Bob The Bot", kahoot_pin="1987040", kahoot_url="https://create.kahoot.it/details/ac258fbd-2d30-4dd8-a7b9-12a1509523cf", is_realistic=True)
